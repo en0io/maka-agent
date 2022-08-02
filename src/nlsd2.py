f010467f0a4cf85edd427a2a9cc9a3a80e7641a1
Alyx Wijers <alyxwijers@gmail.com>
2022-08-01 20:35:42 -0700

    Update nlsd2.py
    increased resilliency

--- src/nlsd2.py
+++ src/nlsd2.py
@@ -11,19 +11,21 @@ from sh import tail
 
 
 def trackufw():
-    for line in tail("-F", "/var/log/ufw.log", _iter=True):
-        log_line_encoded = base64.b64encode(line.encode("utf-8"))
-        payload = {'key': authkey, 'log': log_line_encoded}
-        r = requests.post(ingest_server + '/api/v1/logevent/ufw', data=payload)
-        print("ufw: "+r.text)
+    while True:
+        for line in tail("-F", "/var/log/ufw.log", _iter=True):
+            log_line_encoded = base64.b64encode(line.encode("utf-8"))
+            payload = {'key': authkey, 'log': log_line_encoded}
+            r = requests.post(ingest_server + '/api/v1/logevent/ufw', data=payload)
+            print("ufw: " + r.text)
 
 
 def tracksshd():
-    for line in tail("-F", "/var/log/fail2ban.log", _iter=True):
-        log_line_encoded = base64.b64encode(line.encode("utf-8"))
-        payload = {'key': authkey, 'log': log_line_encoded}
-        r = requests.post(ingest_server + '/api/v2/logevent/sshd', data=payload)
-        print("sshd: "+r.text)
+    while True:
+        for line in tail("-F", "/var/log/fail2ban.log", _iter=True):
+            log_line_encoded = base64.b64encode(line.encode("utf-8"))
+            payload = {'key': authkey, 'log': log_line_encoded}
+            r = requests.post(ingest_server + '/api/v2/logevent/sshd', data=payload)
+            print("sshd: " + r.text)
 
 
 threading.Thread(target=trackufw).start()
